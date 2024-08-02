055b066011ed7b506152856d7a26ebab4ca2692a
Rene <rene.golz84@gmail.com>
2024-08-02 12:32:25 +0200

    added version number

--- jobscheduler.sh
+++ jobscheduler.sh
@@ -18,24 +18,24 @@ debug() {
 
 # Function to get the IP address
 get_ip_address() {
-if [ -n "$(uname -o | grep Android)" ]; then
-  # For Android
-  # First try without 'su'
-  ip=$(ip -4 addr show | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | grep -v 127.0.0.1)
-  if [ -z "$ip" ]; then
-    # If no IP address was found, try with 'ifconfig' and 'su'
-    ip=$(su -c "ifconfig" 2>/dev/null | grep -oP '(?<=inet addr:)\d+(\.\d+){3}' | grep -v 127.0.0.1)
+  if [ -n "$(uname -o | grep Android)" ]; then
+    # For Android
+    # First try without 'su'
+    ip=$(ip -4 addr show | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | grep -v 127.0.0.1)
     if [ -z "$ip" ]; then
-      if su -c true 2>/dev/null; then
-        # SU rights are available
-        ip=$(su -c "ip -4 addr show | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | grep -v 127.0.0.1")
+      # If no IP address was found, try with 'ifconfig' and 'su'
+      ip=$(su -c "ifconfig" 2>/dev/null | grep -oP '(?<=inet addr:)\d+(\.\d+){3}' | grep -v 127.0.0.1)
+      if [ -z "$ip" ]; then
+        if su -c true 2>/dev/null; then
+          # SU rights are available
+          ip=$(su -c "ip -4 addr show | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | grep -v 127.0.0.1")
+        fi
       fi
     fi
+  else
+    # For other Unix systems
+    ip=$(ip -4 -o addr show | awk '$2 !~ /lo|docker/ {print $4}' | cut -d "/" -f 1 | head -n 1)
   fi
-else
-  # For other Unix systems
-  ip=$(ip -4 -o addr show | awk '$2 !~ /lo|docker/ {print $4}' | cut -d "/" -f 1 | head -n 1)
-fi
   echo $ip
 }
 
@@ -88,6 +88,7 @@ else
 fi
 
 # Debugging output
+debug "Version: $VERSION"
 debug "Response from API: $response"
 
 # Parse response
