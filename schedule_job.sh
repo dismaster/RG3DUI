3201bd090a088e7591399caf6fb4e455444d3281
Rene <rene.golz84@gmail.com>
2024-08-27 09:11:01 +0200

    Create schedule_job.sh

---
+++ schedule_job.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+## This File is for running jobscheduler and monitoring in a separated screen session
+## screen -dmS Scheduler ./schedule_job.sh
+
+while true; do
+    ./jobscheduler.sh -debug
+    ./monitor.sh
+    sleep 60
+done
