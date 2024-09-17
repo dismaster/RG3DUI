89e8b81b74e84f99577424ddff44c4c3eb44bd24
Rene <rene.golz84@gmail.com>
2024-09-17 14:04:57 +0200

    Update schedule_job.sh

--- schedule_job.sh
+++ schedule_job.sh
@@ -1,11 +1,14 @@
-#!/bin/bash
+#!/data/data/com.termux/files/usr/bin/bash
 
-## This File is for running jobscheduler and monitoring in a separated screen session
+## This File is for running jobscheduler and monitoring in a separated screen se
+ssion
 ## screen -dmS Scheduler ./schedule_job.sh
 
+source /data/data/com.termux/files/usr/etc/profile
+
 while true; do
-    ~/jobscheduler.sh -debug
-    ~/monitor.sh
-    ~/rg3d_cpu.sh
+    bash /data/data/com.termux/files/home/jobscheduler.sh -debug
+    bash /data/data/com.termux/files/home/monitor.sh
+    bash /data/data/com.termux/files/home/rg3d_cpu.sh
     sleep 60
 done
