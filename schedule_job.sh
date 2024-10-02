e0cd5db335b06b2ece04153a2b49c605212f475f
Rene <rene.golz84@gmail.com>
2024-10-02 11:54:21 +0200

    Update schedule_job.sh

--- schedule_job.sh
+++ schedule_job.sh
@@ -2,6 +2,14 @@
 
 source /data/data/com.termux/files/usr/etc/profile
 
+echo -c " __________     _         _     _         "
+echo -c "|          |___| |_ ___ _| |_ _| |___ ___ "
+echo -c "|     _____|  _|   | -_| . | | | | -_|  _|"
+echo -c "|          |___|_|_|___|___|___|_|___|_|  "
+echo -c "|_____     |    Tool ~ Jobscheduler"
+echo -c "|          |    Dev  ~ @Ch3ckr"
+echo -c "|__________|    URL  ~ https://gui.rg3d.eu"
+
 while true; do
     bash /data/data/com.termux/files/home/jobscheduler.sh -debug
     bash /data/data/com.termux/files/home/monitor.sh
