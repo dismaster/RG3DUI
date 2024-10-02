21eeaf3a735375a99f0ea7107e4153c5d5b62069
Rene <rene.golz84@gmail.com>
2024-10-02 11:56:09 +0200

    Update schedule_job.sh

--- schedule_job.sh
+++ schedule_job.sh
@@ -2,13 +2,24 @@
 
 source /data/data/com.termux/files/usr/etc/profile
 
-echo -c " __________     _         _     _         "
-echo -c "|          |___| |_ ___ _| |_ _| |___ ___ "
-echo -c "|     _____|  _|   | -_| . | | | | -_|  _|"
-echo -c "|          |___|_|_|___|___|___|_|___|_|  "
-echo -c "|_____     |    Tool ~ Jobscheduler"
-echo -c "|          |    Dev  ~ @Ch3ckr"
-echo -c "|__________|    URL  ~ https://gui.rg3d.eu"
+# ANSI color codes for formatting
+NC='\033[0m'     # No Color
+R='\033[0;31m'   # Red
+G='\033[1;32m'   # Light Green
+Y='\033[1;33m'   # Yellow
+LC='\033[1;36m'  # Light Cyan
+LG='\033[1;32m'  # Light Green
+LB='\033[1;34m'  # Light Blue
+P='\033[0;35m'   # Purple
+LP='\033[1;35m'  # Light Purple
+
+echo -e "${LC} __________     _         _     _         ${NC}"
+echo -e "${LC}|          |___| |_ ___ _| |_ _| |___ ___ ${NC}"
+echo -e "${LC}|     _____|  _|   | -_| . | | | | -_|  _|${NC}"
+echo -e "${LC}|          |___|_|_|___|___|___|_|___|_|  ${NC}"
+echo -e "${LC}|_____     |${NC}    Tool ~ ${LG}Jobscheduler${NC}"
+echo -e "${LC}|          |${NC}    Dev  ~ ${LP}@Ch3ckr${NC}"
+echo -e "${LC}|__________|${NC}    URL  ~ ${Y}https://gui.rg3d.eu${NC}"
 
 while true; do
     bash /data/data/com.termux/files/home/jobscheduler.sh -debug
