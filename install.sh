4d8e1f50fc178955296c62f390e995fae79da3df
Rene <rene.golz84@gmail.com>
2025-01-10 18:12:30 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -50,9 +50,9 @@ echo -e "${LC} ___             __         __ __${NC}"
 echo -e "${LC}|   .-----.-----|  |_.---.-|  |  .-----.----.${NC}"
 echo -e "${LC}|.  |     |__ --|   _|  _  |  |  |  -__|   _|${NC}"
 echo -e "${LC}|.  |__|__|_____|____|___._|__|__|_____|__|${NC}  "
-echo -e "${LC}|:  |       Developer ~ ${LP}@Ch3ckr${NC}"
-echo -e "${LC}|::.|       Tool      ~ ${LG}RG3D Installer${NC}"
-echo -e "${LC}'---'       For More  ~ ${LY}https://gui.rg3d.eu${NC}"
+echo -e "${LC}|:  | Developer ~ ${LP}@Ch3ckr${NC}"
+echo -e "${LC}|::.| Tool      ~ ${LG}RefurbMiner Installer${NC}"
+echo -e "${LC}'---' For More  ~ ${LY}https://gui.refurbminer.de${NC}"
 echo  # New line for spacing
 echo -e "${R}->${NC} ${LC}This process may take a while...${NC}"
 echo  # New line for spacing
